3a1821d563dac132900cdb17b9a64716bf26dd31
Akshay Amare <amareakshay@gmail.com>
2020-04-13 22:21:35 +0100

    Implemented Aware interfaces

--- SpringDemo/src/org/akshay/demo/DrawingApplication.java
+++ SpringDemo/src/org/akshay/demo/DrawingApplication.java
@@ -1,20 +1,26 @@
 package org.akshay.demo;
 
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
-		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		//BeanFactory factory = new XmlBeanFactory(new ClassPathResource("spring.xml"));
 		/*
 		 * When using class path application context the spring.xml needs to be in class path
 		 * Hence We need to move it to the classpath in the src folder
 		 */
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println("Getting the Triangle Bean in the Main Application now");
 		//Triangle triangle = (Triangle) factory.getBean("triangle");
+		
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		
 		
